37b392aa28f5853f6d1576db3fdaacb3547b7e39
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2011-07-05 18:44:00 +0200

    changing from canvas to plain html+images, due to bug in android 2.1 canvas implementation

--- engine2.js
+++ engine2.js
@@ -1,9 +1,5 @@
 (function(engine, window) {
-    var canvas = document.getElementById("canvas"), 
-        ctx = canvas.getContext("2d"),
-        w = window.innerWidth,
-        h = window.innerHeight,
-        storyLine = {},
+   var storyLine = {},
         lineheight,
         wordspace,
         currentImage,
@@ -11,29 +7,15 @@
         screen, defaultPage;
 
     function init() {
-    /*
-        var rotate = false,
-            t;
-            */
-        canvas.width = w;
-        canvas.height = h;
-
-/*
-        // if we are in portrait mode, force landscape
-        if(h>w) {
-            rotate = true;
-            ctx.rotate(Math.PI/2);
-            ctx.translate(0, -w)
-            t = w; w = h; h = t;
-        }
-        */
+        w = $("#bg").width();
+        h = $("#bg").height();
 
         // handle touch and click events fast
         // TODO: compatibility
         if ('ontouchstart' in document.documentElement) {
-            canvas.addEventListener('touchstart', handleEvent);
+            document.addEventListener('touchstart', handleEvent);
         } else {
-            canvas.addEventListener('mousedown', handleEvent);
+            document.addEventListener('mousedown', handleEvent);
         }
 
         function handleEvent(e) {
@@ -61,50 +43,24 @@
 
     var show = engine.show = function(screenName) {
         screen = storyLine[screenName];
+        $("#options").text("");
+        $("#desc").text("");
         console.log("show", screenName, screen);
         if(!screen) {
             alert('Internal error: "' + screenName + '" does not exist.');
             return show(defaultPage);
         }
-
-        currentImage = new Image();
-        currentImage.onload = function() {
-            ctx.drawImage(currentImage, 0, 0, 2338, 1665, 0, 0, w, h);
-            setTimeout(textShow(screen.text), 1000);
-        }
-        currentImage.src = "images/" + screenName + ".jpg";
-        currentImage.onerror = function() {
-            alert('Internal error: could not load "' +currentImage.src + '"');
-            return show(defaultPage);
-        }
+        $("#bg").attr("src", "images/" + screenName + ".jpg");
+        textShow(screen.text);
     }
 
     function textShow(text) {
         text = text.split(" ");
-        var xmin = w/12,
-            xmax = w-xmin,
-            x = xmin,
-            y = xmin,
-            textpos = 0;
-        ctx.font = (0|(.9*lineheight)) + "px sans-serif";
-        console.log("font", ctx.font);
+        var textpos = 0;
 
         function showWord() {
             var word = text[textpos];
-            var wordlength = ctx.measureText(word).width + wordspace;
-
-            ctx.fillStyle = "#fff";
-
-            if(wordlength + x > xmax) {
-                x = xmin;
-                y += lineheight;
-                ctx.fillRect(x-wordspace, y - lineheight + 2, wordspace, lineheight);
-            }
-
-            ctx.fillRect(x, y - lineheight + 4, wordlength, lineheight);
-            ctx.fillStyle = "#000";
-            ctx.fillText(word, x, y);
-            x += wordlength;
+            $("#text").text($("#text").text() + " " + word);
 
             if(++textpos < text.length) {
                 showWordTimeout = setTimeout(showWord, Math.random() * 200);
@@ -115,22 +71,29 @@
         nextFn = actions;
     }
 
+    function drawText(x,y,text) {
+        var $text = $("<div>").text(text);
+        $("#options").append($text);
+        $text.css("position", "absolute")
+             .css("top", (y - $text.height() / 2) + "px")
+             .css("left", (x - $text.width() / 2) + "px");
+    }
+
     function actions() {
-        clearTimeout(showWordTimeout);
-        //ctx.drawImage(currentImage, 0, 0, w, h);
-        ctx.drawImage(currentImage, 0, 0, 2338, 1665, 0, 0, w, h);
         console.log("actions", screen);
+
+        clearTimeout(showWordTimeout);
+        $("#text").text("");
+        $("#options").html();
+
         var next = screen.next;
-        var desc = next[0];
-        ctx.fillText(desc, wordspace, h - 4);
+        $("#desc").text(next[0]);
         for(var i = 1; i < next.length; ++i) {
             var x = next[i][0],
                 y = next[i][1],
                 label = next[i][2];
-            console.log(ctx.measureText(label).width);
-            ctx.fillText(label, w*x - ctx.measureText(label).width / 2, h*y - lineheight / 2);
+                drawText(x*w,y*h,label);
         }
-
         nextFn = clickButton;
     }
 
